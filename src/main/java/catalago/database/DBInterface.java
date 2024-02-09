f7b2b056088762556b5256d31269c9e14b889d3f
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-09 20:05:29 -0300

    create DBInterface.java

---
+++ src/main/java/catalago/database/DBInterface.java
@@ -0,0 +1,14 @@
+package catalago.database;
+
+import catalago.entities.Filme;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DBInterface<T,I> {
+    void insert(T entity) throws SQLException;
+    void removeById(I entityID) throws SQLException;
+    T getByID(I entityID) throws SQLException;
+    List<T> getAll() throws SQLException;
+    List<T> getByName(String name) throws SQLException;
+}
