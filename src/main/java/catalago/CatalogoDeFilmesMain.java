de48a6ce8046a5c5815ee748a3f8b95309a65586
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-11 14:46:02 -0300

    adding mvn

--- src/main/java/catalago/CatalogoDeFilmesMain.java
+++ src/main/java/catalago/CatalogoDeFilmesMain.java
@@ -1,12 +1,8 @@
 package catalago;
 
-import catalago.service.AtorService;
-import catalago.service.DiretorService;
-import catalago.service.FilmeService;
 import catalago.service.MenuService;
 
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class CatalogoDeFilmesMain {
     public static void main(String[] args) throws SQLException {
