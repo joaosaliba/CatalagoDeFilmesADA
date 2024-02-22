6fd3a9151fcac9fd415c741bddafc40806f192fb
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-21 21:52:57 -0300

    add shutdownhool

--- src/main/java/catalago/utils/ScannerSingleton.java
+++ src/main/java/catalago/utils/ScannerSingleton.java
@@ -6,6 +6,14 @@ public class ScannerSingleton {
     private Scanner scanner ;
     private static final ScannerSingleton SINGLETON = new ScannerSingleton();
 
+    static{
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                ScannerSingleton.instance().getScanner().close();
+            }
+        });
+    }
     private ScannerSingleton() {
         this.scanner= new Scanner(System.in);
     }
